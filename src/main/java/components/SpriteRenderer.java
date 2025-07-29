8800a94dffaae96316c3183aa2f33ba101ef91bd
Aarav Agarwal <107698264+AaravDevBoi@users.noreply.github.com>
2025-07-29 08:56:12 -0400

    Added RenderBatching, and cleaned up LevelEditorScene to be more clean

--- src/main/java/components/SpriteRenderer.java
+++ src/main/java/components/SpriteRenderer.java
@@ -1,9 +1,15 @@
 package components;
 
 import jade.Component;
+import org.joml.Vector4f;
 
 public class SpriteRenderer extends Component {
-    private boolean firstTime = false;
+
+    Vector4f color;
+
+    public SpriteRenderer(Vector4f color) {
+        this.color = color;
+    }
 
     @Override
     public void start() {
@@ -12,9 +18,10 @@ public class SpriteRenderer extends Component {
 
     @Override
     public void update(float dt) {
-        if (!firstTime) {
-            System.out.println("Updating Sprite Renderer");
-            firstTime = true;
-        }
+
+    }
+
+    public Vector4f getColor() {
+        return this.color;
     }
 }
